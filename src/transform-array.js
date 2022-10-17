0a82e0144cdc4849ea2025f11efb1c526002b7a6
Slawik <peretsviach@gmail.com>
2022-10-17 21:35:33 +0200

    feat: completed task transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(!Array.isArray(arr)){
+    throw new Error(`'arr' parameter must be an instance of the Array!`)
+  } else if ( arr.length === 0){
+    return []
+  }
+
+  let newArray = [];
+
+  for(let i = 0; i < arr.length; i++){
+    if (arr[i] != '--discard-next' && arr[i] != '--discard-prev' && arr[i] != '--double-next' && arr[i] != '--double-prev') {
+      newArray.push(arr[i])
+    } else if (arr[i] === '--discard-next') {
+        if(i === arr.length - 1){
+          continue
+        }
+        i += 1
+    } else if (arr[i] === '--discard-prev') {
+      if(i === 0 || arr[i - 2] === '--discard-next'){
+        continue
+      }
+      
+      newArray.pop()
+    } else if (arr[i] === '--double-next') {
+      if (i === arr.length - 1 || typeof(arr[i + 1]) === 'string'){
+        continue
+      }
+      newArray.push(arr[i + 1])
+    } else if(arr[i] === '--double-prev'){
+      if(i === 0 || typeof(arr[i - 1]) === 'string' || arr[i - 2] === '--discard-next'){
+        continue
+      }
+      newArray.push(arr[i - 1])
+    }
+    
+    
+  }
+  return newArray
 }
 
 module.exports = {
