fde9ed5b1aadd2ca075118d1b6dbc989c87cc91d
Slawik <peretsviach@gmail.com>
2022-10-17 22:30:51 +0200

    feat: completed task repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let array = [];
+
+  if(options){
+    let repeatTimes = options.repeatTimes || 1;
+    let separator = options.separator || '+';
+    let addition = String(options.addition) || '';
+    let additionRepeatTimes = options.additionRepeatTimes || 1;
+    let additionSeparator = options.additionSeparator || '|';
+    let substr = str + ((String(options.addition) !== 'undefined' ? addition + additionSeparator : '')).repeat(additionRepeatTimes);
+    if (String(options.addition) !== 'undefined') {
+      substr = substr.slice(0, substr.length - additionSeparator.length);
+    }
+
+    for (let i = 0; i < repeatTimes; i++) {
+      array.push(substr)
+    }
+    return array.join(separator);
+  }
 }
 
 module.exports = {
